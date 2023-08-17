ac9897ce7c4f6f79a01283f9bd227c79aacd0051
David G. Moore, Jr <david@dgmjr.io>
2023-08-16 20:42:22 -0400

    ADDED TextSummarizationExample function
    - Created a new function called TextSummarization This function an example of text summarization Text Analytics client
    - It takes a TextAnalyticsClient as a parameter and performs text analysis operations such as sentiment analysis and key phrase extraction
    - The function accepts a document string and prepares it for analysis using the client's StartAnalyzeActionsAsync method
    - The operation is then awaited and the results are displayed, including the extracted summary sentences for the document
    
    UPDATED: RunTheTest function
    
    - Modified the RunTheTest function
     Changed the completion parameters to generate a complete implementation of an IIncrementalGenerator in C#
    - Added detailed requirements for the implementation, including looking for decorated classes, structs, and interfaces, and generating partial interfaces for public member

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -26,113 +26,115 @@ namespace Example
         private static readonly AzureKeyCredential credentials = new(languageKey);
         private static readonly Uri endpoint = new(languageEndpoint);
 
-        /// <summary>
-        /// This function demonstrates an example of text summarization using the Text Analytics client
-        /// in C#.
-        /// </summary>
-        /// <param name="client">TextAnalyticsClient is a class provided by the Azure
-        /// Cognitive Services Text Analytics API that allows developers to access the API's text
-        /// analysis capabilities, such as sentiment analysis, key phrase extraction, and text
-        /// summarization. It requires an API key and endpoint to be instantiated.</param>
-        static async Task TextSummarizationExample(TextAnalyticsClient client)
-        {
+        // /// <summary>
+        // /// This function demonstrates an example of text summarization using the Text Analytics client
+        // /// in C#.
+        // /// </summary>
+        // /// <param name="client">TextAnalyticsClient is a class provided by the Azure
+        // /// Cognitive Services Text Analytics API that allows developers to access the API's text
+        // /// analysis capabilities, such as sentiment analysis, key phrase extraction, and text
+        // /// summarization. It requires an API key and endpoint to be instantiated.</param>
+        // static async Task TextSummarizationExample(TextAnalyticsClient client)
+        // {
 
-            string document =
-                @"The extractive summarization feature uses natural language processing techniques to locate key sentences in an unstructured text document.
-                These sentences collectively convey the main idea of the document. This feature is provided as an API for developers.
-                They can use it to build intelligent solutions based on the relevant information extracted to support various use cases.
-                In the public preview, extractive summarization supports several languages. It is based on pretrained multilingual transformer models, part of our quest for holistic representations.
-                It draws its strength from transfer learning across monolingual and harness the shared nature of languages to produce models of improved quality and efficiency.";
+        //     string document =
+        //         @"The extractive summarization feature uses natural language processing techniques to locate key sentences in an unstructured text document.
+        //         These sentences collectively convey the main idea of the document. This feature is provided as an API for developers.
+        //         They can use it to build intelligent solutions based on the relevant information extracted to support various use cases.
+        //         In the public preview, extractive summarization supports several languages. It is based on pretrained multilingual transformer models, part of our quest for holistic representations.
+        //         It draws its strength from transfer learning across monolingual and harness the shared nature of languages to produce models of improved quality and efficiency.";
 
-            // Prepare analyze operation input. You can add multiple documents to this list and perform the same
-            // operation to all of them.
-            var batchInput = new List<string> { document };
+        //     // Prepare analyze operation input. You can add multiple documents to this list and perform the same
+        //     // operation to all of them.
+        //     var batchInput = new List<string> { document };
 
-            var actions = new TextAnalyticsAction
-            {
-                ExtractSummaryActions = new List<ExtractSummaryAction>
-                {
-                    new ExtractSummaryAction()
-                }
-            };
+        //     var actions = new TextAnalyticsAction
+        //     {
+        //         ExtractSummaryActions = new List<ExtractSummaryAction>
+        //         {
+        //             new ExtractSummaryAction()
+        //         }
+        //     };
 
-            // Start analysis process.
-            AnalyzeActionsOperation operation = await client.StartAnalyzeActionsAsync(
-                batchInput,
-                actions
-            );
-            await operation.WaitForCompletionAsync();
-            // View operation status.
-            WriteLine($"AnalyzeActions operation has completed");
-            WriteLine();
+        //     // Start analysis process.
+        //     AnalyzeActionsOperation operation = await client.StartAnalyzeActionsAsync(
+        //         batchInput,
+        //         actions
+        //     );
+        //     await operation.WaitForCompletionAsync();
+        //     // View operation status.
+        //     WriteLine($"AnalyzeActions operation has completed");
+        //     WriteLine();
 
-            WriteLine($"Created On   : {operation.CreatedOn}");
-            WriteLine($"Expires On   : {operation.ExpiresOn}");
-            WriteLine($"Id           : {operation.Id}");
-            WriteLine($"Status       : {operation.Status}");
+        //     WriteLine($"Created On   : {operation.CreatedOn}");
+        //     WriteLine($"Expires On   : {operation.ExpiresOn}");
+        //     WriteLine($"Id           : {operation.Id}");
+        //     WriteLine($"Status       : {operation.Status}");
 
-            WriteLine();
-            // View operation results.
-            await foreach (AnalyzeActionsResult documentsInPage in operation.Value)
-            {
-                IReadOnlyCollection<ExtractSummaryActionResult> summaryResults =
-                    documentsInPage.ExtractSummaryResults;
+        //     WriteLine();
+        //     // View operation results.
+        //     await foreach (AnalyzeActionsResult documentsInPage in operation.Value)
+        //     {
+        //         IReadOnlyCollection<ExtractSummaryActionResult> summaryResults =
+        //             documentsInPage.ExtractSummaryResults;
 
-                foreach (ExtractSummaryActionResult summaryActionResults in summaryResults)
-                {
-                    if (summaryActionResults.HasError)
-                    {
-                        WriteLine($"  Error!");
-                        WriteLine(
-                            $"  Action error code: {summaryActionResults.Error.ErrorCode}."
-                        );
-                        WriteLine($"  Message: {summaryActionResults.Error.Message}");
-                        continue;
-                    }
+        //         foreach (ExtractSummaryActionResult summaryActionResults in summaryResults)
+        //         {
+        //             if (summaryActionResults.HasError)
+        //             {
+        //                 WriteLine($"  Error!");
+        //                 WriteLine(
+        //                     $"  Action error code: {summaryActionResults.Error.ErrorCode}."
+        //                 );
+        //                 WriteLine($"  Message: {summaryActionResults.Error.Message}");
+        //                 continue;
+        //             }
 
-                    foreach (
-                        ExtractSummaryResult documentResults in summaryActionResults.DocumentsResults
-                    )
-                    {
-                        if (documentResults.HasError)
-                        {
-                            WriteLine($"  Error!");
-                            WriteLine(
-                                $"  Document error code: {documentResults.Error.ErrorCode}."
-                            );
-                            WriteLine($"  Message: {documentResults.Error.Message}");
-                            continue;
-                        }
+        //             foreach (
+        //                 ExtractSummaryResult documentResults in summaryActionResults.DocumentsResults
+        //             )
+        //             {
+        //                 if (documentResults.HasError)
+        //                 {
+        //                     WriteLine($"  Error!");
+        //                     WriteLine(
+        //                         $"  Document error code: {documentResults.Error.ErrorCode}."
+        //                     );
+        //                     WriteLine($"  Message: {documentResults.Error.Message}");
+        //                     continue;
+        //                 }
 
-                        WriteLine(
-                            $"  Extracted the following {documentResults.Sentences.Count} sentence(s):"
-                        );
-                        WriteLine();
+        //                 WriteLine(
+        //                     $"  Extracted the following {documentResults.Sentences.Count} sentence(s):"
+        //                 );
+        //                 WriteLine();
 
-                        foreach (SummarySentence sentence in documentResults.Sentences)
-                        {
-                            WriteLine($"  Sentence: {sentence.Text}");
-                            WriteLine();
-                        }
-                    }
-                }
-            }
-        }
+        //                 foreach (SummarySentence sentence in documentResults.Sentences)
+        //                 {
+        //                     WriteLine($"  Sentence: {sentence.Text}");
+        //                     WriteLine();
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
 
         private static async Task RunTheTest(OpenAIClient client)
         {
             var completion = new CompletionsOptions();
-            completion.Prompts.Add("You're such a wanker!");
-            completion.ChoicesPerPrompt = 10;
-            completion.MaxTokens = 10;
+            completion.Prompts.Add(@"Write for me a complete implementation of an IIncrementalGenerator in C# which does the following:
+            - Looks for classes, structs, and interfaces which are decorated with the [DecomposeAttribute] and take no parameters or an an assembly which is decorated with the [DecomposeAttribute] and taked exactly one parameter of type Type.
+            - For each public member in each class, struct, interface decorated with the DecomposeAttribute or specified in the constructor of the Assembly-decorating attribute, it should generate a public partial interface  called I[Type][Member], which implements only that public member");
+            completion.ChoicesPerPrompt = 1;
+            completion.MaxTokens = 4000;
             completion.User = "DGMJR";
 
 
             var result = await client.GetCompletionsAsync("GPT-35-turbo", completion);
 
-            foreach (var choicee in result.Value.Choices)
+            foreach (var choice in result.Value.Choices)
             {
-                Console.WriteLine(choicee.Text);
+                Console.WriteLine(choice.Text);
             }
         }
 
